32389f5195d129dd1672017f5eac9fd59afef637
Enric Moreu <enric.moreu.filella@gmail.com>
2019-11-22 20:00:42 +0000

    Monitor working, ram, ssd, hdd and cpu

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# launchpad-monitor
+# Launchpad Monitor
+
+Reuse your Novation Launchpad to check your desk performance!
+
+![Alt text](launchpad.jpg?raw=true)
+
+## Installation
+
+1. Check your MIDI device name:
+`amidi -l`
+
+2. Install the following packages:
+`sudo apt-get install libjack0 libjack-dev libasound2-dev`
+
+3. Install the following Python2.7 packages:
+`sudo pip install rtmidi psutil mido python-rtmidi`
+
+## More info
+
+https://d2xhy469pqj8rc.cloudfront.net/sites/default/files/novation/downloads/4080/launchpad-programmers-reference.pdf
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import mido
 import psutil
-outport = mido.open_output('Launchpad S:Launchpad S MIDI 1 20:0')
+import time
+import subprocess
+outport = mido.open_output('Launchpad S:Launchpad S MIDI 1')
 
 core_positions = [
     [70, 60, 50, 40],
@@ -13,6 +15,11 @@ core_positions = [
     [77, 67, 57, 47]
 ]
 
+ram_positions = [0,1,2,3,4,5,6,7]
+
+ssd_positions = [10,11,12,13,14,15,16,17]
+hdd_positions = [20,21,22,23,24,25,26,27]
+
 def all_on():
     outport.send(mido.Message.from_hex('B0 00 7F'))
 def all_off():
@@ -21,25 +28,50 @@ def enable_led(position, color):
     outport.send(mido.Message.from_hex('90 {} {}'.format(position, color)))
 def print_cores():
     values = psutil.cpu_percent(interval=1, percpu=True)
-    print(values)
-    for i in range(7):
-        # import pdb;pdb.set_trace()
+    for i in range(8):
         value = map_to_core(values[i])
         for j in range(value):
-            print(i,j)
             enable_led(str(core_positions[i][j]), '0f')
-        # core_positions[i]
 
+def print_ram():
+    ram = dict(psutil.virtual_memory()._asdict())['used']/(1024*1024*1024)
+    for position in ram_positions:
+        if position*4 < ram:
+            enable_led(str(position).zfill(2), '3C')
 
-    # outport.send(mido.Message.from_hex('90 00 0f'))
+def print_disk():
+    ssd, hdd = get_disk_space()
+    for ssd_position in ssd_positions:
+        if 10*(ssd_position-10) < ssd*0.75:
+            enable_led(str(ssd_position).zfill(2), '3E')
+    
+    for hdd_position in hdd_positions:
+        if 10*(hdd_position-20) < hdd*0.75:
+            enable_led(str(hdd_position).zfill(2), '3F')
+
+def get_disk_space():
+    diskinfo_raw = subprocess.Popen("df -h", shell=True,stdout=subprocess.PIPE)
+    output = diskinfo_raw.communicate()[0]
+    output_dict = dict((fields[5], fields[4]) for fields in [line.split() for line in output.strip().split("\n")][1:])
+    ssd = output_dict['/'].rstrip('%')
+    hdd = output_dict['/media/enric/enric_hdd'].rstrip('%')
+    
+    return int(ssd), int(hdd)
+
 def map_to_core(value):
     if value < 25:
         return 0
     elif value >= 25 and value < 50:
-        return 1
-    elif value >= 50 and value < 75:
         return 2
-    else:
+    elif value >= 50 and value < 75:
         return 3
+    else:
+        return 4
 
-print_cores()
+while True:
+    print_disk()
+    get_disk_space()
+    print_cores()
+    print_ram()
+    time.sleep(5)
+    all_off()
